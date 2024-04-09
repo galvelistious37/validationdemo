c4801a709b4704f9b70051f9fe592f8dd5a5e7ea
johnny <john.robertson88@yahoo.com>
2024-04-08 23:50:24 -0700

    Add regex validation annotation

--- src/main/java/com/johnny/spring/validationdemo/model/Customer.java
+++ src/main/java/com/johnny/spring/validationdemo/model/Customer.java
@@ -1,9 +1,6 @@
 package com.johnny.spring.validationdemo.model;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public class Customer {
 
@@ -16,6 +13,9 @@ public class Customer {
     @NotNull(message="is required")
     @Size(min=1, message="is required")
     private String lastName = "";
+
+    @Pattern(regexp="^[a-zA-Z0-9]{5}", message="only 5 chars/digits")
+    private String postalCode;
 
     public String getFirstName() {
         return firstName;
@@ -39,5 +39,13 @@ public class Customer {
 
     public void setFreePasses(int freePasses) {
         this.freePasses = freePasses;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
     }
 }

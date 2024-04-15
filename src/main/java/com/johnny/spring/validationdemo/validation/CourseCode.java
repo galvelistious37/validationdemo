718d2afa98a8ad0ff2fc646b079420f12b7995a8
johnny <john.robertson88@yahoo.com>
2024-04-14 21:31:45 -0700

    Create a custom annotation and bind it to a variable. Check input variable against the custom annotation and display custom message if check fails

---
+++ src/main/java/com/johnny/spring/validationdemo/validation/CourseCode.java
@@ -0,0 +1,27 @@
+package com.johnny.spring.validationdemo.validation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Constraint(validatedBy = CourseCodeConstraintValidator.class)
+@Target({ElementType.METHOD, ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface CourseCode {
+
+    // Define default course code
+    public String value() default "JEK";
+
+    // Define default error message
+    public String message() default "Must start with JEK";
+
+    // Define default groups
+    public Class<?>[] groups() default {};
+
+    // Define default payloads
+    public Class<? extends Payload>[] payload() default {};
+}
